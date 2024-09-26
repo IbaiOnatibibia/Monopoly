399efb91497b156a6c6b290038ec06347cea55f9
IBAI <IBAI@10.245.68.252>
2024-09-26 11:12:00 +0200

    Card comment added

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,7 +1,7 @@
 package edu.ncsu.monopoly;
 //updated augost 24 with a comment
 public abstract class Card {
-
+//I hace added a comment
     public static final int TYPE_CHANCE = 1;
     public static final int TYPE_CC = 2;
 
